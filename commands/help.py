a23b7b947d84e3e4dd2a098c08f6c6232ef04564
vua tro choi <tqchinh92@gmail.com>
2025-11-05 11:47:30 +0700

    Add files via upload

--- commands/help.py
+++ commands/help.py
@@ -1,25 +1,6 @@
 name = "help"
+import os
 
 async def run(message, args):
-    
-    full_msg = message.content.strip()
-    prefix = ""
-    if full_msg and not full_msg[0].isalnum():
-        prefix = full_msg[0] 
-    else:
-        prefix = "$"
-
-    help_text = (
-        f"  **Danh sách lệnh hiện có:**\n"
-        f"`{prefix}help` — Hiển thị bảng lệnh\n"
-        f"`{prefix}ping` — Sống không bot ơi\n"
-        f"`{prefix}add <từ1> <từ2>` — Thêm cặp từ mới\n"
-        f"`{prefix}whelp <từ>` — Hiển thị 10 từ có thể nối tiếp\n"
-        f"`{prefix}thelp` — Từ điển ngôn ngữ phổ biến <ngôn ngữ đích>\n"
-        f"`{prefix}gay` — Check gay\n"
-        f"`{prefix}translate <ngôn_ngữ_đích> <văn bản>` — Dịch ngôn ngữ\n"
-        f"`{prefix}pick` — Bot chọn hộ\n"
-        f"`{prefix}tientri` — Đưa ra một lời tiên tri\n"
-    )
-
-    await message.channel.send(help_text)
+    all_cmds = [f[:-3] for f in os.listdir("commands") if f.endswith(".py")]
+    await message.channel.send(f"**Lệnh có sẵn:** {', '.join(all_cmds)}")
